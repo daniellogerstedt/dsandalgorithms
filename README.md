fc18c4ab344016bb1dcbc936c33024079da173f7
Daniel Logerstedt <dl@Daniels-MBP.localdomain>
2018-11-26 18:12:45 -0800

    commit for completion of problem for Nov 26th

---
+++ README.md
@@ -0,0 +1,7 @@
+#Data Structures and Algorithms Problems:
+
+###November 26th: Reverse an array.
+Inline-style:
+![solution image](./assets/reverseArray.jpg)
+
+Uses a method built to reverse string arrays. This method works by looping half way through the array reversing it by swapping indexes based on the value of "i"
--- src/main/java/ArrayReverse.java
+++ src/main/java/ArrayReverse.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class ArrayReverse {
     public static void main (String[] args) {
-        System.out.println("hello world");
+        String[] arr1 = new String[4];
+        arr1[0] = "a";
+        arr1[1] = "b";
+        arr1[2] = "c";
+        arr1[3] = "d";
+        System.out.println(Arrays.toString(arr1));
+        arr1 = reverse(arr1);
+        System.out.println(Arrays.toString(arr1));
     }
+
+    public static String[] reverseArray(String[] arr) {
+        for (int i = 0; i < arr.length/2; i++ ) {
+            String temp = arr[arr.length-1-i];
+            arr[arr.length-1-i] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
 }
