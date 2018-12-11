9d60d1ccfa100a2dcaa00d69d6fdbc51f1ad90fe
Daniel Logerstedt <dl@Daniels-MacBook-Pro.local>
2018-12-11 09:08:00 -0800

    finished building solution and tests. wrote readme for methods.

---
+++ src/main/java/QueueWithStacks/QueueWithStacks.java
@@ -0,0 +1,39 @@
+package QueueWithStacks;
+
+public class QueueWithStacks<T> {
+
+    protected Stack<T> inStack;
+    protected Stack<T> outStack;
+
+    public QueueWithStacks() {
+        this.inStack = new Stack<T>();
+        this.outStack = new Stack<T>();
+    }
+
+    public void enqueue (T val) {
+        //takes in Value of type T Generic and passes it into the in stack.
+        this.inStack.push(val);
+    }
+
+    public Node<T> dequeue() {
+        this.cycleStacks();
+        return this.outStack.pop();
+    }
+
+    public T peek() {
+        this.cycleStacks();
+        return this.outStack.top != null? this.outStack.top.value: null;
+    }
+
+    private void cycleStacks () {
+        // Helper Method that dumps in stack into out stack if out is empty.
+        if (this.outStack.top == null) {
+            Node<T> curr = this.inStack.top;
+            while (curr != null) {
+                this.outStack.push(this.inStack.pop().value);
+                curr = curr.next;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/QueueWithStacks/Stack.java
@@ -0,0 +1,26 @@
+package QueueWithStacks;
+
+public class Stack<T> {
+    protected Node<T> top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    public T peek() {
+        if(this.top == null) return null;
+        return this.top.value;
+    }
+
+    public void push(T v) {
+        Node<T> node = new Node<T>(v, this.top);
+        this.top = node;
+    }
+
+    public Node<T> pop() {
+        Node<T> node = this.top;
+        this.top = this.top.next;
+        return node;
+    }
+
+}
