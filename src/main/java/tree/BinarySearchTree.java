a5bd61360cd653f5e4c727b064f333bede1e3116
Daniel Logerstedt <dl@Daniels-MBP.localdomain>
2018-12-17 10:40:12 -0800

    built binary tree subclass with add and search built tests for binary tree subclass methods

---
+++ src/main/java/tree/BinarySearchTree.java
@@ -0,0 +1,36 @@
+package tree;
+
+public class BinarySearchTree extends Tree {
+
+    public void add (int v) {
+        if (this.root == null) {
+            Node<Integer> node = new Node<>(v);
+            this.root = node;
+        } else {
+            Node curr = this.root;
+            while (curr != null) {
+                if ((int)curr.value < v) {
+                    if (curr.right == null) {
+                        curr.right = new Node<>(v);
+                        return;
+                    }
+                    else curr = curr.right;
+                }if ((int)curr.value > v) {
+                    if (curr.left == null) {
+                        curr.left = new Node<>(v);
+                        return;
+                    }
+                    else curr = curr.left;
+                }
+            }
+        }
+    }
+
+    public Node search (int v) {
+        Node curr = this.root;
+        while (curr != null && (int)curr.value != v) {
+            curr = (int)curr.value > v? curr.left : curr.right;
+        }
+        return curr;
+    }
+}
---
+++ src/test/java/tree/BinarySearchTreeTest.java
@@ -0,0 +1,61 @@
+package tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTreeTest {
+
+    @Test
+    public void testConstructor() {
+        Tree testBinary = new BinarySearchTree();
+        assertNull(testBinary.root);
+    }
+
+    @Test
+    public void testAdd() {
+        BinarySearchTree testBinary = new BinarySearchTree();
+        testBinary.add(10);
+        assertNotNull(testBinary.root);
+        assertNull(testBinary.root.left);
+        assertNull(testBinary.root.right);
+        assertEquals(10, testBinary.root.value);
+        testBinary.add(5);
+        assertNull(testBinary.root.right);
+        assertNull(testBinary.root.left.left);
+        assertNull(testBinary.root.left.right);
+        assertEquals(5, testBinary.root.left.value);
+        testBinary.add(15);
+        assertNull(testBinary.root.left.left);
+        assertNull(testBinary.root.left.right);
+        assertNull(testBinary.root.right.left);
+        assertNull(testBinary.root.right.right);
+        assertEquals(15, testBinary.root.right.value);
+    }
+
+    @Test
+    public void testSearch() {
+        BinarySearchTree testBinary = new BinarySearchTree();
+        testBinary.add(5);
+        testBinary.add(3);
+        testBinary.add(4);
+        testBinary.add(2);
+        testBinary.add(7);
+        testBinary.add(6);
+        testBinary.add(8);
+        Node testRoot = testBinary.search(5);
+        Node testRootL = testBinary.search(3);
+        Node testRootLL = testBinary.search(2);
+        Node testRootLR = testBinary.search(4);
+        Node testRootR = testBinary.search(7);
+        Node testRootRL = testBinary.search(6);
+        Node testRootRR = testBinary.search(8);
+        assertEquals(testBinary.root, testRoot);
+        assertEquals(testBinary.root.left, testRootL);
+        assertEquals(testBinary.root.right, testRootR);
+        assertEquals(testBinary.root.left.left, testRootLL);
+        assertEquals(testBinary.root.left.right, testRootLR);
+        assertEquals(testBinary.root.right.left, testRootRL);
+        assertEquals(testBinary.root.right.right, testRootRR);
+    }
+}
